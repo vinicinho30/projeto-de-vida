2534861702535016b545df81f28b356a5b7ab62f
Afrodite <106694662+vinicinho30@users.noreply.github.com>
2024-03-06 14:20:47 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+conat botoes= document .querySelectorAll(".botao);
 
+                                         for(let i=0;i <botes.length;i++){
+botoes[i].onclick = function(){
+
+  for(let j=0;j<botoes.length;++){
+    botoes[j].classlist.remove("ativo);
+                               }
+
+  botoes[i].vlaList.add("ativo")
+}
+}
+ 
