172cff428260c0794dbfeef3274def3ab86fbb0e
Afrodite <106694662+vinicinho30@users.noreply.github.com>
2024-03-06 14:24:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-conat botoes= document .querySelectorAll(".botao);
+const botoes = document.querySelectorAll(".botao");
 
-                                         for(let i=0;i <botes.length;i++){
-botoes[i].onclick = function(){
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
 
-  for(let j=0;j<botoes.length;++){
-    botoes[j].classlist.remove("ativo);
-                               }
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
 
-  botoes[i].vlaList.add("ativo")
+        botoes[i].classList.add("ativo");
+    }
 }
-}
- 
