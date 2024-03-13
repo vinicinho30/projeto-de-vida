7d3eed501555a09afc3f3d3639dffc016889ac8b
Afrodite <106694662+vinicinho30@users.noreply.github.com>
2024-03-13 15:21:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,6 +15,9 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-02-30T00:00:00");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 let tempoAtual = new Date();
 
