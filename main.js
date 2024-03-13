13069490af562826d3e51e5e05b1790785472ac1
Afrodite <106694662+vinicinho30@users.noreply.github.com>
2024-03-13 14:33:58 -0300

    Update main.js

--- main.js
+++ main.js
@@ -19,3 +19,11 @@ const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
